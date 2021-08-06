083b76679f0d4de04a0f80ef2a355926325aeb49
Naman <namansharma232009@gmail.com>
2021-08-06 09:50:40 +0530

    fixed typo and added docs

--- newline.py
+++ newline.py
@@ -1,4 +1,6 @@
 a = 10
 b = 20
 c = 50
-print(a, '/n', b , '/n', c)
+print(a, '/n', b,  '/n', c)
+# /n gives newline
+# it can be used in only string data type
--- tabspace.py
+++ tabspace.py
@@ -1,4 +1,6 @@
 a = 10
 b = 20
 c = 50
-print(a, '/t', b , '/t', c)
+print(a, '/t', b, '/t', c)
+# /t gives tab space (4 spaces)
+# it can be used in only string data type
